726302570ebfbd372eea6040d6ee3d8b514f43e1
best-time <349057996@qq.com>
2017-08-06 22:17:49 +0800

    base.js

--- jsFrame/base.js
+++ jsFrame/base.js
@@ -76,6 +76,19 @@
   });
 });        
 
+    // 刷新页面的几种方法
+    function reload() {
+        history.go(0)
+        location.reload()
+        location=location
+        location.assign(location)
+        document.execCommand('Refresh')
+        window.navigate(location)
+        location.replace(location)
+        document.URL=location.href
+    }
+
+
 
 function nameSpace(str, obj) {
             var parts = str.split('.'), 
